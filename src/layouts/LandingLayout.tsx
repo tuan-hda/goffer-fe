7963d92e632d97813309d38d68a525ad03b30eda
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-02-26 23:56:52 +0700

    Add animation add spring effect

--- src/layouts/LandingLayout.tsx
+++ src/layouts/LandingLayout.tsx
@@ -6,13 +6,18 @@ const LandingLayout = () => {
         <div className='h-screen w-full'>
             <Outlet />
             <div className='absolute bottom-12 flex flex-col items-center justify-center w-full'>
-                <Tabs classNames={{ tabList: 'bg-beige shadow-xl' }} size='lg' color='primary'>
-                    <Tab key='home' title='Home' href='/' />
-                    <Tab key='who-we-are' title='Who we are' href='/who-we-are' />
-                    <Tab key='features' title='Features' href='/features' />
-                    <Tab key='trusted-by' title='Trusted by' href='/trusted-by' />
-                    <Tab key='pricing' title='Pricing' href='/pricing' />
-                </Tabs>
+                <div className='flex items-center'>
+                    <Tabs classNames={{ tabList: 'bg-beige shadow-xl relative z-10' }} size='lg' color='primary'>
+                        <Tab key='home' title='Home' href='/' />
+                        <Tab key='who-are-we' title='Who are we' href='/who-we-are' />
+                        <Tab key='features' title='Features' href='/features' />
+                        <Tab key='trusted-by' title='Trusted by' href='/trusted-by' />
+                        <Tab key='pricing' title='Pricing' href='/pricing' />
+                    </Tabs>
+                    <div className='rounded-r-xl cursor-pointer bg-gray-50 hover:bg-gray-100 transition h-11 flex items-center -ml-5 relative w-[120px] text-gray-600 pl-10 shadow-xl font-semibold'>
+                        Sign Up
+                    </div>
+                </div>
                 <div className='flex items-center justify-center gap-4 mt-3'>
                     <Link to='/about-us' className='hover:underline hover:text-text transition text-sm text-black/40'>
                         About us
