062c047324d6040daa745b5175dc3f83691def5b
Phan Thanh Tú <pttu2902@gmail.com>
2024-03-03 16:36:02 +0700

    fix: refactor and create applicant header

--- src/configs/routes.tsx
+++ src/configs/routes.tsx
@@ -1,20 +1,22 @@
-import { RouteObject } from "react-router-dom";
-import { LandingPage, NotFound } from "../pages";
-import LandingLayout from "../layouts/LandingLayout";
-import WhoAreWe from "../pages/WhoAreWe";
-import { HomePage } from "../pages/applicant";
+import { RouteObject } from 'react-router-dom';
+import { LandingPage, NotFound } from '../pages';
+import WhoAreWe from '../pages/WhoAreWe';
+import { ApplicantLayout, LandingLayout } from '../layouts';
 
 const routesConfig: RouteObject[] = [
-  { path: "/applicant/find-work", element: <HomePage /> },
-  {
-    path: "/",
-    element: <LandingLayout />,
-    children: [
-      { path: "/", element: <LandingPage /> },
-      { path: "who-are-we", element: <WhoAreWe /> },
-    ],
-  },
-  { path: "*", element: <NotFound /> },
+    {
+        path: '/',
+        element: <LandingLayout />,
+        children: [
+            { path: '/', element: <LandingPage /> },
+            { path: 'who-are-we', element: <WhoAreWe /> },
+        ],
+    },
+    { path: '*', element: <NotFound /> },
+    {
+        path: '/applicant',
+        element: <ApplicantLayout />,
+    },
 ];
 
 export default routesConfig;
