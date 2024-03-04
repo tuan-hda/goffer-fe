f9baa9b78f46915e0d36f5f2bf29eaf00ae9fe54
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-03-04 23:43:59 +0700

    modulize into LandingTwoColumn for resuable stuffs

--- src/layouts/LandingTwoColumn.tsx
+++ src/layouts/LandingTwoColumn.tsx
@@ -2,6 +2,8 @@ import { useAnimation, motion } from 'framer-motion';
 import { ReactElement, useEffect, useMemo } from 'react';
 import { fadeInPropsFn, zoomInPropsFn } from '../utils/animation';
 
+const SPEED_FACTOR = 1;
+
 type WhoAreWeProps = {
     title: string;
     left: {
@@ -28,8 +30,8 @@ const LandingTwoColumn = ({ left, right, title, rightAlt }: WhoAreWeProps) => {
     }, [ctrls]);
 
     const getAnimationProps = (animType: 'fadeIn' | 'zoomIn', i: number) => {
-        if (animType === 'fadeIn') return getFadeInProps(i * 0.5 + 0.5);
-        else return getZoomInProps(i * 0.5 + 0.5);
+        if (animType === 'fadeIn') return getFadeInProps(i * SPEED_FACTOR + 0.5);
+        else return getZoomInProps(i * SPEED_FACTOR + 0.5);
     };
 
     return (
@@ -49,7 +51,7 @@ const LandingTwoColumn = ({ left, right, title, rightAlt }: WhoAreWeProps) => {
                     <div className="flex-1 min-h-0" />
 
                     <motion.div
-                        {...getFadeInProps(3)}
+                        {...getFadeInProps(left.length * SPEED_FACTOR + 1)}
                         className="flex items-center gap-2 font-light text-sm text-black/40 mt-6 invert"
                     >
                         Press <img src="/space-button.svg" alt="space-bar" className="w-16 opacity-50" /> or use arrow
