47c7e9df9974edd38e6988e02d3ee230b6961776
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-03-13 08:44:46 +0700

    Finish partially sign up flow

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,5 +2,5 @@ import { SignUpRequest } from 'src/types/user.type';
 import { noAuthAxios } from './base';
 
 export const signUpService = async (user: SignUpRequest) => {
-    return noAuthAxios.post('/auth/sign-up', user);
+    return noAuthAxios.post('/auth/register', user);
 };
