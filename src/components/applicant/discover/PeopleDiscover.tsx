15691b35ff3e77ed04f1ef8b5a9f047935b7f428
Phan Thanh Tú <pttu2902@gmail.com>
2024-03-14 23:32:48 +0700

    fix: complete sidebar

--- src/components/applicant/discover/PeopleDiscover.tsx
+++ src/components/applicant/discover/PeopleDiscover.tsx
@@ -3,7 +3,7 @@ import PersonCard from '../common/PersonCard';
 const PeopleDiscover = () => {
     const peoples = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
     return (
-        <div className="py-8 mx-auto grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-rows-4">
+        <div className="mx-auto grid grid-cols-1 gap-8 py-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-rows-4">
             {peoples.map((person, index) => (
                 <PersonCard key={index} />
             ))}
