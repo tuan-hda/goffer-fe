816658d1a90e98a893b22c17c3a8d1f15d32d6a4
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-03-25 22:16:39 +0700

    add user popover

--- src/components/navigation/UserPopover.tsx
+++ src/components/navigation/UserPopover.tsx
@@ -1,6 +1,16 @@
 import { Avatar } from '@nextui-org/react';
 import classNames from 'classnames';
 import useSelfProfileQuery from 'src/hooks/useSelfProfileQuery';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from 'src/components/ui/dropdown-menu';
+import { TbHomePlus } from 'react-icons/tb';
+import { Button } from '../ui/button';
 
 type UserPopoverProps = {
     collapsed: boolean;
@@ -10,17 +20,50 @@ const UserPopover = ({ collapsed }: UserPopoverProps) => {
     const { data: user } = useSelfProfileQuery();
 
     return (
-        <button className="relative -mx-0.5 mb-5 mt-7 flex w-full items-center gap-3 rounded-lg p-2 transition hover:bg-gray-100">
-            <Avatar className="h-7 w-7" src={user?.avatar} />
-            <p
-                className={classNames(
-                    'pointer-events-auto absolute left-12 overflow-hidden whitespace-nowrap opacity-100 transition',
-                    collapsed ? 'pointer-events-none !opacity-0' : 'pointer-events-auto opacity-100',
-                )}
-            >
-                {user?.name}
-            </p>
-        </button>
+        <DropdownMenu>
+            <DropdownMenuTrigger className="mb-5 mt-7 w-full outline-none ring-0">
+                <button className="relative -mx-0.5 flex w-full items-center gap-3 rounded-lg p-2 transition hover:bg-gray-100">
+                    <Avatar className="h-7 w-7" src={user?.avatar} />
+                    <p
+                        className={classNames(
+                            'pointer-events-auto absolute left-12 overflow-hidden whitespace-nowrap opacity-100 transition',
+                            collapsed ? 'pointer-events-none !opacity-0' : 'pointer-events-auto opacity-100',
+                        )}
+                    >
+                        {user?.name}
+                    </p>
+                </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-[240px] rounded-xl">
+                <DropdownMenuLabel>Individual</DropdownMenuLabel>
+                <DropdownMenuItem>
+                    <button className="relative -mx-0.5 flex w-full items-center gap-3 rounded-lg px-1 py-0.5 transition">
+                        <Avatar className="h-7 w-7" src={user?.avatar} />
+                        <p
+                            className={classNames(
+                                'pointer-events-auto overflow-hidden whitespace-nowrap opacity-100 transition',
+                                collapsed ? 'pointer-events-none !opacity-0' : 'pointer-events-auto opacity-100',
+                            )}
+                        >
+                            {user?.name}
+                        </p>
+                    </button>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuLabel>Organizations</DropdownMenuLabel>
+                <p className="px-2 text-left text-sm text-text/60">You have no organizations.</p>
+                <div className="mb-2 mt-[10px] px-2">
+                    <Button
+                        size="sm"
+                        variant="outline"
+                        className="flex w-full items-center gap-2 text-sm transition hover:text-text"
+                    >
+                        <TbHomePlus className="text-base" />
+                        <span>New organization</span>
+                    </Button>
+                </div>
+            </DropdownMenuContent>
+        </DropdownMenu>
     );
 };
 
