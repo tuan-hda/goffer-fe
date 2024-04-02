3247c0bda04ec4cdc0e2d95c0187eb77044d2643
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-04-02 09:25:06 +0700

    Finish UI of new job and fix cmdk error

--- src/components/newJob/EditExperience.tsx
+++ src/components/newJob/EditExperience.tsx
@@ -1,11 +1,7 @@
-import classNames from 'classnames';
 import { useState } from 'react';
-import { TbCheck } from 'react-icons/tb';
-import { Button } from '@/components/ui/button';
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from '@/components/ui/command';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
 
-const frameworks = [
+const experienceList = [
     {
         value: '<1 year',
         label: '<1 year',
@@ -33,46 +29,18 @@ const EditExperience = () => {
     const [value, setValue] = useState('<1 year');
 
     return (
-        <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger className="flex-1" asChild>
-                <Button
-                    variant="outline"
-                    role="combobox"
-                    aria-expanded={open}
-                    className="w-full justify-between rounded-lg"
-                >
-                    {value
-                        ? frameworks.find((framework) => framework.value === value)?.label
-                        : 'Choose experience range...'}
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-[556px] rounded-lg p-0">
-                <Command>
-                    <CommandInput placeholder="Work anytime" className="h-9" />
-                    <CommandEmpty>No experience range found</CommandEmpty>
-                    <CommandGroup>
-                        {frameworks.map((framework) => (
-                            <CommandItem
-                                key={framework.value}
-                                value={framework.value}
-                                onSelect={(currentValue) => {
-                                    setValue(currentValue === value ? '' : currentValue);
-                                    setOpen(false);
-                                }}
-                            >
-                                {framework.label}
-                                <TbCheck
-                                    className={classNames(
-                                        'ml-auto h-4 w-4',
-                                        value === framework.value ? 'opacity-100' : 'opacity-0',
-                                    )}
-                                />
-                            </CommandItem>
-                        ))}
-                    </CommandGroup>
-                </Command>
-            </PopoverContent>
-        </Popover>
+        <Select>
+            <SelectTrigger className="flex-1">
+                <SelectValue defaultValue={'<1 year'} />
+            </SelectTrigger>
+            <SelectContent>
+                {experienceList.map((exp) => (
+                    <SelectItem key={exp.label} value={exp.value}>
+                        {exp.label}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
     );
 };
 
