f6db8ff2e44798945f5c000e0024d80d96c0dbe9
Phan Thanh Tu <pttu2902@gmail.com>
2024-04-05 17:53:05 +0700

    update layout

--- src/components/applicant/discover/job/JobDiscover.tsx
+++ src/components/applicant/discover/job/JobDiscover.tsx
@@ -1,10 +1,23 @@
-import classNames from 'classnames';
-import JobCard from '../../common/JobCard';
-import JobDetail from './JobDetail';
-import JobFilter from '../../filter/JobFilter';
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from '@/components/ui/sheet';
 import useJobStore from '@/stores/jobStore';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import { Divider } from '@nextui-org/react';
+import JobCard from '../../common/JobCard';
 
 const jobs = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
@@ -23,19 +36,46 @@ const JobDiscover = () => {
     }, [location, updateTabKey]);
 
     return (
-        <div className="flex flex-col">
-            <JobFilter />
-            <div className="flex w-full max-w-screen-xl flex-1 flex-col gap-y-4 p-4 xl:mx-auto">
+        <div className="flex">
+            <div className="flex flex-col gap-x-4">
                 {jobs.map((job, index) => (
-                    <div
-                        key={index}
-                        className={classNames('flex justify-center', jobDetailOpening ? 'w-1/2 pr-4' : 'w-full')}
-                    >
-                        <JobCard />
-                    </div>
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            {/* <Button className="h-fit overflow-hidden bg-transparent p-0 hover:bg-pale/50"> */}
+                            <JobCard />
+                            {/* </Button> */}
+                        </SheetTrigger>
+                        <SheetContent side={'bottom'}>
+                            <SheetHeader>
+                                <SheetTitle>Edit profile</SheetTitle>
+                                <SheetDescription>
+                                    Make changes to your profile here. Click save when you're done.
+                                </SheetDescription>
+                            </SheetHeader>
+                            <div className="grid gap-4 py-4">
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="name" className="text-right">
+                                        Name
+                                    </Label>
+                                    <Input id="name" value="Pedro Duarte" className="col-span-3" />
+                                </div>
+                                <div className="grid grid-cols-4 items-center gap-4">
+                                    <Label htmlFor="username" className="text-right">
+                                        Username
+                                    </Label>
+                                    <Input id="username" value="@peduarte" className="col-span-3" />
+                                </div>
+                            </div>
+                            <SheetFooter>
+                                <SheetClose asChild>
+                                    <Button type="submit">Save changes</Button>
+                                </SheetClose>
+                            </SheetFooter>
+                        </SheetContent>
+                    </Sheet>
                 ))}
             </div>
-            <JobDetail />
+            <Divider orientation="vertical" />
         </div>
     );
 };
