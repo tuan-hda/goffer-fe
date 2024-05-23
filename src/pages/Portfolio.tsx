fa1e60162b3c6cd72be6fb9bb5971fb3b9768311
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-05-23 10:42:29 +0700

    Add portfolio list

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,5 +1,21 @@
+import { TemplateList } from '@/components/portfolio';
+import { BreadcrumbItem, Breadcrumbs } from '@nextui-org/react';
+import { TbPaint } from 'react-icons/tb';
+
 const Portfolio = () => {
-    return <div>Portfolio</div>;
+    return (
+        <div className="px-6 py-5 text-sm">
+            <Breadcrumbs className="mt-[5px]">
+                <BreadcrumbItem>
+                    <TbPaint className="text-lg" /> Portfolio
+                </BreadcrumbItem>
+            </Breadcrumbs>
+            <div className="pt-5">
+                <h1 className="mb-7 text-3xl">Choose a template</h1>
+                <TemplateList />
+            </div>
+        </div>
+    );
 };
 
 export default Portfolio;
