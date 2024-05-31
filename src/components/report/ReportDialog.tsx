7571dd2e0638ec6d65bec3d3a012203803117c5f
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-05-31 09:53:57 +0700

    Add report button and form

---
+++ src/components/report/ReportDialog.tsx
@@ -0,0 +1,51 @@
+import { TbLoader } from 'react-icons/tb';
+import {
+    AlertDialog,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '../ui/alert-dialog';
+import { Button } from '../ui/button';
+
+type ReportSubmitProps = {
+    children: React.ReactNode;
+    img?: string | null;
+    loading?: boolean;
+};
+
+const ReportDialog = ({ children, img, loading }: ReportSubmitProps) => {
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+            <AlertDialogContent className="max-h-[90vh] w-full max-w-[1000px] overflow-y-auto p-7">
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Report issue</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        Please help us fill out the form below to report the issue.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+
+                {loading ? (
+                    <div className="flex min-h-[50vh] w-full">
+                        <TbLoader className="m-auto animate-spin text-2xl" />
+                    </div>
+                ) : img ? (
+                    <img src={img} className="rounded-xl border" />
+                ) : (
+                    <span className="text-sm">There some error capturing the screenshot. Please try again.</span>
+                )}
+
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <Button variant="black">Submit</Button>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+};
+
+export default ReportDialog;
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,35 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import useSelfProfileQuery from '@/hooks/useSelfProfileQuery';
-import { Button } from '@/components/ui/button';
-import { TbFlag } from 'react-icons/tb';
-import { createPortal } from 'react-dom';
 import { useRef } from 'react';
-import { toPng } from 'html-to-image';
+import ReportButton from '@/components/report/ReportButton';
 
 const RootLayout = () => {
     const ref = useRef<HTMLDivElement>(null);
-
-    const captureScreenshot = async () => {
-        const dataUrl = await toPng(ref.current as HTMLElement);
-        const img = new Image();
-        img.src = dataUrl;
-        const a = document.createElement('a');
-        a.href = dataUrl;
-        a.download = 'screenshot.png';
-        document.body.appendChild(a);
-        a.click();
-    };
 
     useSelfProfileQuery();
     return (
         <div className="h-full w-full" ref={ref}>
             <Outlet />
-            {createPortal(
-                <Button onClick={captureScreenshot} variant="outline" className="fixed bottom-2 right-2 z-[100000]">
-                    <TbFlag className="mr-2" /> Report
-                </Button>,
-                document.body,
-            )}
+            <ReportButton containerRef={ref} />
         </div>
     );
 };
