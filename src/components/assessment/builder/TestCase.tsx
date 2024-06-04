2d340fc55643ab68dd9abc4f7050ec818e940607
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-06-04 23:34:12 +0700

    add option to show test case as example

--- src/components/assessment/builder/TestCase.tsx
+++ src/components/assessment/builder/TestCase.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
-import { TbX } from 'react-icons/tb';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { TbCheck, TbCircle, TbX } from 'react-icons/tb';
 
 type TestCaseProps = {
     num: number;
@@ -13,9 +14,26 @@ const TestCase = ({ num, onRemove }: TestCaseProps) => {
             <p className="mr-2">{num}.</p>
             <Textarea placeholder="Input" className="flex-1" />
             <Textarea placeholder="Output" className="flex-1" />
-            <Button onClick={onRemove} variant="ghost" size="icon">
-                <TbX />
-            </Button>
+            <div className="flex flex-col">
+                <Button onClick={onRemove} variant="ghost" size="icon">
+                    <TbX />
+                </Button>
+                <TooltipProvider>
+                    <Tooltip>
+                        <TooltipTrigger>
+                            <Button variant="ghost" size="icon">
+                                <TbCircle className="text-gray-500/10" />
+                            </Button>
+                            {/* <Button variant="ghost" size="icon">
+                                <TbCheck />
+                            </Button> */}
+                        </TooltipTrigger>
+                        <TooltipContent>
+                            <p>Show as example for examinee</p>
+                        </TooltipContent>
+                    </Tooltip>
+                </TooltipProvider>
+            </div>
         </div>
     );
 };
