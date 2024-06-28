4493d0ef62e206cfa6ffc093a8e2635e7d035868
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-06-28 15:51:31 +0700

    attempt to reduce size

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +21,7 @@ export default defineConfig({
         visualizer({
             open: true,
         }),
+        ViteMinifyPlugin({}),
     ],
     server: {
         port: 5173,
