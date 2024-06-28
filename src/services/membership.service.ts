4936c588265ba78695f303e3fee748e56d5b7bc1
Phan Thanh Tu <pttu2902@gmail.com>
2024-06-28 18:05:28 +0700

    Update membership.service.ts

--- src/services/membership.service.ts
+++ src/services/membership.service.ts
@@ -1,8 +1,14 @@
 import { Membership, NewMembership } from '@/types/membership.type';
 import { baseAxios } from './base';
+import { List } from '@/types/list.type';
+import { ListQueryOptions } from '@/types/common.type';
 
 export const addMemberService = async (data: NewMembership) => {
     return (await baseAxios.post<Membership>('/memberships', data)).data;
+};
+
+export const listMemberService = async (query?: Partial<Record<keyof (Membership & ListQueryOptions), string>>) => {
+    return (await baseAxios.get<List<Membership>>('/memberships', { params: query })).data;
 };
 
 export const getOrgMembers = async (orgId: string) => {
