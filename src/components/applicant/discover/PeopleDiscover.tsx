1eca8742d316fa85325e753842c66aab29da6b2d
tuan-hda <anhtuan.hoangdinh@gmail.com>
2024-07-13 15:25:25 +0700

    view people using route instead

--- src/components/applicant/discover/PeopleDiscover.tsx
+++ src/components/applicant/discover/PeopleDiscover.tsx
@@ -25,13 +25,10 @@ const PeopleDiscover = () => {
         <>
             <div className="mx-auto grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
                 {users.map((person) => (
-                    <Sheet key={person.id} onOpenChange={async (open) => !open && (await refetch())}>
-                        <PersonCard data={person} />
+                    // <Sheet key={person.id} onOpenChange={async (open) => !open && (await refetch())}>
+                    <PersonCard data={person} key={person.id} />
 
-                        <SheetContent className="!max-w-screen-lg overflow-y-auto p-8 pr-0">
-                            <UserDetail id={person.id} />
-                        </SheetContent>
-                    </Sheet>
+                    // </Sheet>
                 ))}
             </div>
             {/* <div className="mt-14 flex w-full flex-col justify-center">
