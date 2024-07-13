116d8eff121ba11250195d48e95d9168dfacb2a5
Phan Thanh Tú <pttu2902@gmail.com>
2024-07-13 17:08:25 +0700

    fix bugs

--- src/components/applicant/common/PersonCard.tsx
+++ src/components/applicant/common/PersonCard.tsx
@@ -8,10 +8,10 @@ import { User } from '@/types/user.type';
 import { getLatestExperience } from '@/utils/profile';
 import { client } from '@/utils/streamchat';
 import { useNavigate } from 'react-router-dom';
-import useListPeople from '@/hooks/useListPeople';
 import { toggleSavedUser } from '@/services/interaction.service';
 import { SheetTrigger } from '@/components/ui/sheet';
 import { useState } from 'react';
+import useUsersRecommender from '@/hooks/useUsersRecommender';
 
 const colors = ['#F8F9FE'];
 
@@ -22,7 +22,7 @@ interface Props {
 const PersonCard = ({ data }: Props) => {
     const navigate = useNavigate();
     const latestExp = getLatestExperience(data.experiences ?? []);
-    const { refetch } = useListPeople();
+    const { refetch } = useUsersRecommender();
 
     const [chatLoading, setChatLoading] = useState(false);
 
--- src/components/orgDetail/Jobs.tsx
+++ src/components/orgDetail/Jobs.tsx
@@ -16,33 +16,35 @@ const Jobs = ({ org }: Props) => {
         <div className="h-full px-4">
             <p className="mb-6 ml-6 mt-12 text-2xl font-semibold text-text">All jobs</p>
 
-            {jobs?.results.map((job) => (
-                <Sheet key={job.id}>
-                    <SheetTrigger asChild>
-                        <Button
-                            key={job.id}
-                            variant="ghost"
-                            className="mb-4 w-full justify-between rounded-2xl px-6 py-10"
-                        >
-                            <div>
-                                <p className="text-start font-semibold text-text">
-                                    {job.title} @ {org.name}
-                                </p>
-                                <p className="text-start text-sm text-muted-foreground">
-                                    <span>{job.location}</span>
-                                    <span className="mx-2 text-lg">•</span>
-                                    <span>${numeral(job.salaryFrom).format('0a')}</span>
-                                    {job.salaryTo && <span> - ${numeral(job.salaryTo).format('0a')}</span>}
-                                </p>
-                            </div>
-                            <TbArrowRight size={20} />
-                        </Button>
-                    </SheetTrigger>
-                    <SheetContent className="!max-w-[900px] overflow-y-auto p-8">
-                        <JobDetail jobId={job.id} />
-                    </SheetContent>
-                </Sheet>
-            ))}
+            {jobs?.results
+                .filter((job) => job.isPublished)
+                .map((job) => (
+                    <Sheet key={job.id}>
+                        <SheetTrigger asChild>
+                            <Button
+                                key={job.id}
+                                variant="ghost"
+                                className="mb-4 w-full justify-between rounded-2xl px-6 py-10"
+                            >
+                                <div>
+                                    <p className="text-start font-semibold text-text">
+                                        {job.title} @ {org.name}
+                                    </p>
+                                    <p className="text-start text-sm text-muted-foreground">
+                                        <span>{job.location}</span>
+                                        <span className="mx-2 text-lg">•</span>
+                                        <span>${numeral(job.salaryFrom).format('0a')}</span>
+                                        {job.salaryTo && <span> - ${numeral(job.salaryTo).format('0a')}</span>}
+                                    </p>
+                                </div>
+                                <TbArrowRight size={20} />
+                            </Button>
+                        </SheetTrigger>
+                        <SheetContent className="!max-w-[900px] overflow-y-auto p-8">
+                            <JobDetail jobId={job.id} />
+                        </SheetContent>
+                    </Sheet>
+                ))}
         </div>
     );
 };
